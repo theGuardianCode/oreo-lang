5320ea878d378c917cd71adb3d4f62c4d71c9c92
theGuardianCode <nickrichard878@gmail.com>
2022-08-05 10:28:11 +1000

    Parser can find left and right values of operator

--- src/parser.cpp
+++ src/parser.cpp
@@ -13,6 +13,7 @@ struct Node {
 
 void parse(std::vector<Token> tokenVec, int length) {
     std::vector<Node> syntaxTree;
-    syntaxTree.push_back({"+", NULL, NULL});
-    std::cout << syntaxTree[0].data << "\n";
+    Node node = {tokenVec[1].value, new Node({tokenVec[0].value, NULL, NULL}), new Node({tokenVec[2].value, NULL, NULL})};
+    syntaxTree.push_back(node);
+    std::cout << syntaxTree[0].left->data << " -> " << syntaxTree[0].data << " <- " << syntaxTree[0].right->data << "\n";
 }
